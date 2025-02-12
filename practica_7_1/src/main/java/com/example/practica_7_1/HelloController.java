94d26b6bda4c79bc5cc376257485525e43114d71
LAALAD <160270603+LAALAD@users.noreply.github.com>
2025-02-12 20:30:29 +0100

    Parte1

---
+++ practica_7_1/src/main/java/com/example/practica_7_1/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.practica_7_1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+
+    protected void onHelloButtonClick() {
+    }
+}
